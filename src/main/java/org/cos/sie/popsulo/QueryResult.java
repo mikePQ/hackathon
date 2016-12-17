d73ba05fe01521da46a7db566cc8c8a2ad817096
Jacek Gołda <strefowy.jacek@gmail.com>
2016-12-17 18:06:14 +0100

    Interfejsy do komunikacji z YouNoob

--- src/main/java/org/cos/sie/popsulo/QueryResult.java
+++ src/main/java/org/cos/sie/popsulo/QueryResult.java
@@ -1,4 +1,20 @@
 package org.cos.sie.popsulo;
 
 public class QueryResult {
+
+	public QueryResult(String title, String author) {
+		this.title = title;
+		this.author = author;
+	}
+
+	private String title;
+	private String author;
+
+	public String getTitle() {
+		return title;
+	}
+
+	public String getAuthor() {
+		return author;
+	}
 }
