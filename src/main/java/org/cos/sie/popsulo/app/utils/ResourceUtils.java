0ff491eeaa0b166cea7b1df5f632984c64e91a37
MikePQ <kontakt@michalperlak.pl>
2016-12-17 17:26:51 +0100

    Key loading added to resource util

--- src/main/java/org/cos/sie/popsulo/app/utils/ResourceUtils.java
+++ src/main/java/org/cos/sie/popsulo/app/utils/ResourceUtils.java
@@ -4,23 +4,27 @@ import org.cos.sie.popsulo.app.utils.resource.UTF8ResourcesControl;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.Locale;
 import java.util.MissingResourceException;
+import java.util.Properties;
 import java.util.ResourceBundle;
 
 public class ResourceUtils {
 
 	private static final Logger logger = LoggerFactory.getLogger(ResourceUtils.class);
+	private static final String PROPERTIES_FILENAME = "Key.properties";
 
-	private static ResourceUtils instace;
+	private static ResourceUtils instance;
 
 	private ResourceBundle messageResources;
 
-	public static ResourceUtils getInstace() {
-		if ( instace == null ) {
-			instace = new ResourceUtils();
+	public static ResourceUtils getInstance() {
+		if ( instance == null ) {
+			instance = new ResourceUtils();
 		}
-		return instace;
+		return instance;
 	}
 
 	private ResourceUtils() {
@@ -34,6 +38,18 @@ public class ResourceUtils {
 		} catch ( MissingResourceException exc ) {
 			throw new IllegalStateException("Could not load resource bundle");
 		}
+	}
+
+	public static Properties getKeyProperties()
+	{
+		Properties properties = new Properties();
+		try {
+			InputStream in = ResourceUtils.class.getResourceAsStream(PROPERTIES_FILENAME);
+			properties.load(in);
+		} catch (IOException e) {
+			logger.error("There was an error reading " + PROPERTIES_FILENAME + ": " + e.getCause() + " : " + e.getMessage());
+		}
+        return properties;
 	}
 
 	public String getItem(String key) {
