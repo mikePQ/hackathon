286703a2ddc837ce7b7783c1cf2acdbef93b8b7a
MikePQ <kontakt@michalperlak.pl>
2016-12-17 13:30:46 +0100

    First video downloaded

---
+++ src/main/java/org/cos/sie/popsulo/DummyDownload.java
@@ -0,0 +1,21 @@
+package org.cos.sie.popsulo;
+
+import com.github.axet.vget.VGet;
+
+import java.io.File;
+import java.net.URL;
+
+public class DummyDownload {
+
+    public static void main(String[] args) {
+        try {
+            String url = "https://www.youtube.com/watch?v=WjrqpJQBKCQ";
+            String path = "D:\\youtube";
+            VGet v = new VGet(new URL(url), new File(path));
+            v.download();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+}
