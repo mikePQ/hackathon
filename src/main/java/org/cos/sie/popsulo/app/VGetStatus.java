af09eca9bf45eb555bd2e80cd5d91922cf52ce75
MikePQ <kontakt@michalperlak.pl>
2016-12-18 02:12:45 +0100

    Player for cached files ver.3

---
+++ src/main/java/org/cos/sie/popsulo/app/VGetStatus.java
@@ -0,0 +1,134 @@
+package org.cos.sie.popsulo.app;
+
+import com.github.axet.vget.info.VideoFileInfo;
+import com.github.axet.vget.info.VideoInfo;
+import com.github.axet.vget.vhs.VimeoInfo;
+import com.github.axet.vget.vhs.YouTubeInfo;
+import com.github.axet.wget.SpeedInfo;
+import com.github.axet.wget.info.DownloadInfo;
+import com.github.axet.wget.info.DownloadInfo.Part;
+import com.github.axet.wget.info.DownloadInfo.Part.States;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class VGetStatus
+    implements Runnable
+{
+    VideoInfo videoinfo;
+
+    long last;
+
+    Map<VideoFileInfo, SpeedInfo> map = new HashMap<VideoFileInfo, SpeedInfo>();
+
+    public VGetStatus(VideoInfo i)
+    {
+        this.videoinfo = i;
+    }
+
+    public SpeedInfo getSpeedInfo(VideoFileInfo dinfo)
+    {
+        SpeedInfo speedInfo = map.get(dinfo);
+        if (speedInfo == null) {
+            speedInfo = new SpeedInfo();
+            speedInfo.start(dinfo.getCount());
+            map.put(dinfo, speedInfo);
+        }
+        return speedInfo;
+    }
+
+    @Override public void run()
+    {
+        List<VideoFileInfo> dinfoList = videoinfo.getInfo();
+
+        // notify app or save download state
+        // you can extract information from DownloadInfo info;
+        switch (videoinfo.getState()) {
+            case EXTRACTING:
+            case EXTRACTING_DONE:
+            case DONE:
+                if (videoinfo instanceof YouTubeInfo) {
+                    YouTubeInfo i = (YouTubeInfo)videoinfo;
+                    System.out.println(videoinfo.getState() + " " + i.getVideoQuality());
+                } else if (videoinfo instanceof VimeoInfo) {
+                    VimeoInfo i = (VimeoInfo)videoinfo;
+                    System.out.println(videoinfo.getState() + " " + i.getVideoQuality());
+                } else {
+                    System.out.println("downloading unknown quality");
+                }
+                for (VideoFileInfo d : videoinfo.getInfo()) {
+                    SpeedInfo speedInfo = getSpeedInfo(d);
+                    speedInfo.end(d.getCount());
+                    System.out.println(String.format("file:%d - %s (%s)", dinfoList.indexOf(d), d.targetFile,
+                        formatSpeed(speedInfo.getAverageSpeed())));
+                }
+                break;
+            case ERROR:
+                System.out.println(videoinfo.getState() + " " + videoinfo.getDelay());
+
+                if (dinfoList != null) {
+                    for (DownloadInfo dinfo : dinfoList) {
+                        System.out.println("file:" + dinfoList.indexOf(dinfo) + " - " + dinfo.getException() + " delay:"
+                            + dinfo.getDelay());
+                    }
+                }
+                break;
+            case RETRYING:
+                System.out.println(videoinfo.getState() + " " + videoinfo.getDelay());
+
+                if (dinfoList != null) {
+                    for (DownloadInfo dinfo : dinfoList) {
+                        System.out.println(
+                            "file:" + dinfoList.indexOf(dinfo) + " - " + dinfo.getState() + " " + dinfo.getException()
+                                + " delay:" + dinfo.getDelay());
+                    }
+                }
+                break;
+            case DOWNLOADING:
+                long now = System.currentTimeMillis();
+                if (now - 1000 > last) {
+                    last = now;
+
+                    String parts = "";
+
+                    for (VideoFileInfo dinfo : dinfoList) {
+                        SpeedInfo speedInfo = getSpeedInfo(dinfo);
+                        speedInfo.step(dinfo.getCount());
+
+                        List<Part> pp = dinfo.getParts();
+                        if (pp != null) {
+                            // multipart download
+                            for (Part p : pp) {
+                                if (p.getState().equals(States.DOWNLOADING)) {
+                                    parts += String.format("part#%d(%.2f) ", p.getNumber(),
+                                        p.getCount() / (float)p.getLength());
+                                }
+                            }
+                        }
+                        System.out.println(
+                            String.format("file:%d - %s %.2f %s (%s)", dinfoList.indexOf(dinfo), videoinfo.getState(),
+                                dinfo.getCount() / (float)dinfo.getLength(), parts,
+                                formatSpeed(speedInfo.getCurrentSpeed())));
+                    }
+                }
+                break;
+            default:
+                break;
+        }
+    }
+
+    public static String formatSpeed(long s)
+    {
+        if (s > 0.1 * 1024 * 1024 * 1024) {
+            float f = s / 1024f / 1024f / 1024f;
+            return String.format("%.1f GB/s", f);
+        } else if (s > 0.1 * 1024 * 1024) {
+            float f = s / 1024f / 1024f;
+            return String.format("%.1f MB/s", f);
+        } else {
+            float f = s / 1024f;
+            return String.format("%.1f kb/s", f);
+        }
+    }
+}
--- src/main/java/org/cos/sie/popsulo/app/controller/PlayerController.java
+++ src/main/java/org/cos/sie/popsulo/app/controller/PlayerController.java
@@ -1,5 +1,10 @@
 package org.cos.sie.popsulo.app.controller;
 
+import com.github.axet.vget.VGet;
+import com.github.axet.vget.info.VGetParser;
+import com.github.axet.vget.info.VideoFileInfo;
+import com.github.axet.vget.info.VideoInfo;
+import com.github.axet.wget.info.ex.DownloadInterruptedError;
 import javafx.application.Platform;
 import javafx.beans.Observable;
 import javafx.beans.property.SimpleStringProperty;
@@ -7,26 +12,24 @@ import javafx.beans.property.StringProperty;
 import javafx.beans.value.ChangeListener;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.Slider;
-import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.layout.VBox;
 import javafx.scene.media.Media;
 import javafx.scene.media.MediaPlayer;
 import javafx.util.Duration;
 import org.cos.sie.popsulo.app.QueryResult;
+import org.cos.sie.popsulo.app.VGetStatus;
 import org.cos.sie.popsulo.app.utils.ResourceUtils;
-import org.cos.sie.popsulo.converter.FormatConverter;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.net.URISyntaxException;
 import java.net.URL;
-import java.text.SimpleDateFormat;
-import java.time.Instant;
-import java.util.Date;
+import java.util.List;
 import java.util.ResourceBundle;
+import java.util.concurrent.atomic.AtomicBoolean;
 
 import static java.lang.Math.floor;
 import static java.lang.String.format;
@@ -36,6 +39,7 @@ public class PlayerController
 {
     private static final Logger logger = LoggerFactory.getLogger(PlayerController.class);
 
+    @FXML private VBox mainPane;
     @FXML private Label timeLabel;
     @FXML private Slider slider;
     @FXML private Label videoIdLabel;
@@ -52,7 +56,6 @@ public class PlayerController
     private Duration duration;
     private MediaPlayer mediaPlayer;
 
-
     @Override public void initialize(URL location, ResourceBundle resources)
     {
         ResourceBundle bundle = ResourceUtils.loadLabelsForDefaultLocale();
@@ -60,24 +63,12 @@ public class PlayerController
         titleBase = bundle.getString("labels.player.video.title");
         authorBase = bundle.getString("labels.player.author");
         dateBase = bundle.getString("labels.player.video.date");
-
-        try {
-            String path = FormatConverter.class.getResource("testInput.mp4").toURI().toString();
-            Image miniatureImage = new Image(getClass().getResource("/icons/mainIcon.png").toString());
-            String videoId = "1111";
-            String title = "ziemniak";
-            String author = "zbyszek";
-            Date date = new Date(Instant.now().toEpochMilli());
-
-            QueryResult result = new QueryResult(videoId, title, author, date, miniatureImage, path);
-            updateState(result);
-        } catch (URISyntaxException e) {
-            logger.error("Cannot initialize player controller", e);
-        }
+        mainPane.setVisible(false);
     }
 
     public void pause()
     {
+
         mediaPlayer.pause();
     }
 
@@ -89,7 +80,7 @@ public class PlayerController
         mediaPlayer.play();
     }
 
-    void updateState(QueryResult result)
+    public void updateState(QueryResult result)
         throws URISyntaxException
     {
         miniatureImageView.setImage(result.getMiniature());
@@ -109,6 +100,7 @@ public class PlayerController
         });
 
         updateProgress();
+        mainPane.setVisible(true);
     }
 
     private void updateMediaPlayer(QueryResult result)
@@ -154,10 +146,32 @@ public class PlayerController
         }
     }
 
-    private static String formatDate(Date date)
+    private String getStreamUrl(String videoId)
     {
-        SimpleDateFormat format = new SimpleDateFormat("dd/MM/YYYY");
-        return format.format(date);
+        try {
+            final AtomicBoolean stop = new AtomicBoolean(false);
+
+            String baseUrl= ""; //TODO change
+            URL web = new URL(baseUrl + videoId);
+            VGetParser user = VGet.parser(web);
+
+            VideoInfo videoinfo = user.info(web);
+            VGet v = new VGet(videoinfo);
+            VGetStatus notify = new VGetStatus(videoinfo);
+            v.extract(user, stop, notify);
+
+            List<VideoFileInfo> list = videoinfo.getInfo();
+            if (list != null) {
+                for (VideoFileInfo d : list) {
+                    return d.getSource().toString();
+                }
+            }
+        } catch (RuntimeException e) {
+            throw e;
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+        throw new IllegalStateException("Cannot find stream url");
     }
 
     private static String formatTime(Duration elapsed, Duration duration)
