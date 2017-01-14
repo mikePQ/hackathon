ecce0975a119163c9c7392c799fe8a31b21431ba
Jacek Gołda <strefowy.jacek@gmail.com>
2017-01-14 20:29:59 +0100

    Even more unused code removed

--- src/main/java/org/cos/sie/popsulo/youtubeSearch/impl/SearchQuery.java
+++ src/main/java/org/cos/sie/popsulo/youtubeSearch/impl/SearchQuery.java
@@ -1,69 +1,64 @@
 package org.cos.sie.popsulo.youtubeSearch.impl;
 
-import com.google.api.client.googleapis.json.GoogleJsonResponseException;
 import com.google.api.client.http.HttpRequest;
 import com.google.api.client.http.HttpRequestInitializer;
+import com.google.api.client.http.HttpTransport;
+import com.google.api.client.http.javanet.NetHttpTransport;
+import com.google.api.client.json.JsonFactory;
+import com.google.api.client.json.jackson2.JacksonFactory;
 import com.google.api.services.youtube.YouTube;
-import com.google.api.services.youtube.model.ResourceId;
 import com.google.api.services.youtube.model.SearchListResponse;
 import com.google.api.services.youtube.model.SearchResult;
-import com.google.api.services.youtube.model.Thumbnail;
-import org.cos.sie.popsulo.Auth;
-import org.cos.sie.popsulo.LocalDiskCache;
 
 import java.io.IOException;
-import java.util.Iterator;
 import java.util.List;
 
-public class SearchQuery
-{
-    private YouTube youtube;
+public class SearchQuery {
+	private static final HttpTransport HTTP_TRANSPORT = new NetHttpTransport();
 
-    private YouTube.Search.List search;
+	private static final JsonFactory JSON_FACTORY = new JacksonFactory();
 
-    private final static String retrivedFields =
-        "items(" + "id/kind," + "id/videoId," + "id/channelId," + "snippet/title," + " snippet/channelTitle,"
-            + " snippet/publishedAt," + " snippet/thumbnails/default/url)";
+	private YouTube.Search.List search;
 
-    private static final String fileName = "YouTubeCache";
+	private final static String retrievedFields =
+			"items(" + "id/kind," + "id/videoId," + "id/channelId," + "snippet/title," + " snippet/channelTitle,"
+					+ " snippet/publishedAt," + " snippet/thumbnails/default/url)";
 
-    private static final String searchedObjectType = "video";
+	private static final String fileName = "YouTubeCache";
 
-    private final static String apiKey = "AIzaSyBnfhEl2amzE41PfLOkQFe4WxvkrDlgtbY";
+	private static final String searchedObjectType = "video";
 
-    public SearchQuery()
-    {
-        youtube = new YouTube.Builder(Auth.HTTP_TRANSPORT, Auth.JSON_FACTORY, new HttpRequestInitializer()
-        {
-            public void initialize(HttpRequest request)
-                throws IOException
-            {
-                //nothing happens when request is initalized
-            }
-        }).setApplicationName(fileName).build();
+	private final static String apiKey = "AIzaSyBnfhEl2amzE41PfLOkQFe4WxvkrDlgtbY";
 
-        try {
-            search = youtube.search().list("id,snippet");
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        search.setKey(apiKey);
-        search.setType(searchedObjectType);
-        search.setFields(retrivedFields);
-    }
+	public SearchQuery() {
+		YouTube youtube = new YouTube.Builder(HTTP_TRANSPORT, JSON_FACTORY, new HttpRequestInitializer() {
+			public void initialize(HttpRequest request)
+					throws IOException {
+				//nothing happens when request is initalized
+			}
+		}).setApplicationName(fileName).build();
 
-    //" Video Id" getLink using : "https://www.youtube.com/watch?v="+ rId.getVideoId()
-    // " Title: " + singleVideo.getSnippet().getTitle()
-    // "Channel title " + singleVideo.getSnippet().getChannelTitle()
-    // "Published Date" + singleVideo.getSnippet().getPublishedAt());
+		try {
+			search = youtube.search().list("id,snippet");
+		} catch ( IOException e ) {
+			e.printStackTrace();
+		}
+		search.setKey(apiKey);
+		search.setType(searchedObjectType);
+		search.setFields(retrievedFields);
+	}
 
-    public List<SearchResult> searchForVideos(String queryTerm, long numberOfVideos)
-        throws IOException
-    {
-        search.setMaxResults(numberOfVideos);
-        search.setQ(queryTerm);
-        SearchListResponse searchResponse = search.execute();
-        return searchResponse.getItems();
-    }
+	//" Video Id" getLink using : "https://www.youtube.com/watch?v="+ rId.getVideoId()
+	// " Title: " + singleVideo.getSnippet().getTitle()
+	// "Channel title " + singleVideo.getSnippet().getChannelTitle()
+	// "Published Date" + singleVideo.getSnippet().getPublishedAt());
+
+	public List<SearchResult> searchForVideos(String queryTerm, long numberOfVideos)
+			throws IOException {
+		search.setMaxResults(numberOfVideos);
+		search.setQ(queryTerm);
+		SearchListResponse searchResponse = search.execute();
+		return searchResponse.getItems();
+	}
 
 }
