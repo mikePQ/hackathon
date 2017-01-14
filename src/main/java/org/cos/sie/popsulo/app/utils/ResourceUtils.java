797c12e1df83939f79143e46218bed5206ecfdd2
Jacek Gołda <strefowy.jacek@gmail.com>
2017-01-14 20:16:48 +0100

    Further unused code removed

--- src/main/java/org/cos/sie/popsulo/app/utils/ResourceUtils.java
+++ src/main/java/org/cos/sie/popsulo/app/utils/ResourceUtils.java
@@ -14,15 +14,6 @@ import java.util.ResourceBundle;
 
 public class ResourceUtils {
 
-	private static final Logger logger = LoggerFactory.getLogger(ResourceUtils.class);
-	private static final String PROPERTIES_FILENAME = "Key.properties";
-
-	private ResourceBundle messageResources;
-
-	private ResourceUtils() {
-		messageResources = loadLabelsForDefaultLocale();
-	}
-
 	public static ResourceBundle loadLabelsForDefaultLocale() {
 		Locale defaultLocale = Locale.forLanguageTag("pl-PL");
 		try {
@@ -30,21 +21,5 @@ public class ResourceUtils {
 		} catch ( MissingResourceException exc ) {
 			throw new IllegalStateException("Could not load resource bundle");
 		}
-	}
-
-	public static Properties getKeyProperties()
-	{
-		Properties properties = new Properties();
-		try {
-			InputStream in = ResourceUtils.class.getResourceAsStream(PROPERTIES_FILENAME);
-			properties.load(in);
-		} catch (IOException e) {
-			logger.error("There was an error reading " + PROPERTIES_FILENAME + ": " + e.getCause() + " : " + e.getMessage());
-		}
-        return properties;
-	}
-
-	public String getItem(String key) {
-		return messageResources.getString(key);
 	}
 }
